f8aeb4040583e0231860e78b5b574d4c8821dcb9
Bruna Flôr <brn.flor@gmail.com>
2022-09-12 21:39:04 +0100

    creating mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,12 @@ import express from "express"
 
 const app = express()
 
-app.get("/ads", (request, response) => {
+app.get("/ads", (request:any, response:any) => {
     return response.json([
         {id:1, name:"Anúncio 1"},
         {id:2, name:"Anúncio 2"},
-        {id:3, name:"Anúncio 3"}
+        {id:3, name:"Anúncio 3"},
+        {id:4, name:"Anúncio 4"}
     ])
 })
 app.listen(3333)
