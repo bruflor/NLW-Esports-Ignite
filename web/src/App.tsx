47ed9e4fa8275c820d9bd31fca5514101e630b7c
Bruna Flor <brn.flor@gmail.com>
2022-09-13 23:21:39 +0100

    wip: main landing page css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,23 @@
 import "./styles/main.css";
-
+import logoImg from "./assets/logo-nlw-esports.png";
 function App() {
-  return <div className="w-8 h-8 bg-violet-500">NLW</div>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="">
+          <img src=".assets/games/game-1.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
