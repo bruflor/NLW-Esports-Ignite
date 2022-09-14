55a15b0582e2a699c03f7b9a39a253fd9eab89a8
Bruna Flor <brn.flor@gmail.com>
2022-09-15 00:19:22 +0100

    finishing cards

---
+++ mobile/src/components/GameCard/index.tsx
@@ -0,0 +1,35 @@
+import {
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ImageBackground,
+  ImageSourcePropType,
+  Text,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { styles } from "./styles";
+import { THEME } from "../../theme";
+
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props extends TouchableOpacity {
+  data: GameCardProps;
+}
+
+export function GameCard({ data, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground source={data.cover} style={styles.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.ads}>{data.ads} anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
