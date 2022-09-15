9a58619592813623cf03aa669e80be43970d1674
Bruna Flor <brn.flor@gmail.com>
2022-09-15 22:24:46 +0100

    functions for convert hours and minutes for *100

---
+++ server/src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,6 @@
+export function convertHourStringToMinutes(houtString: string) {
+    const [hours, minutes] = houtString.split(":").map(Number)
+    const  minutesAmount = (hours * 60 ) + minutes;
+
+    return  minutesAmount
+}
