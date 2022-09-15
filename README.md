b08af748a4d934576035529d85476f6b27a45b9b
Bruna Flor <brn.flor@gmail.com>
2022-09-15 21:24:50 +0100

    Changing http request

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # NLW-Esports-Ignite
 
-A multi-platform application developed from the NLW eSports event by Rocketseat. 
+A multi-platform application developed from the NLW eSports event by Rocketseat.
 The technologies are the following:
 
-Backend:
-  NodeJs
+Backend e DB:
+NodeJs
+SQLite
+Prisma
 
 Frontend:
-  ReactJs
-  TypeScript
-  TailWind
+ReactJs
+TypeScript
+TailWind
 
 Mobile:
-  React Native
-  TypeScript
+React Native
+TypeScript
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,17 @@ import express from "express"
 
 const app = express()
 
-app.get("/ads", (request:any, response:any) => {
+app.get("/games", (request, response) => {
+    return response.json([])
+})
+
+//httpcode status 201 = created
+app.post("/ads", (request, response) => {
+    return response.status(201).json([])
+})
+
+
+app.get("/games/:id/ads", (request:any, response:any) => {
     return response.json([
         {id:1, name:"Anúncio 1"},
         {id:2, name:"Anúncio 2"},
@@ -10,4 +20,14 @@ app.get("/ads", (request:any, response:any) => {
         {id:4, name:"Anúncio 4"}
     ])
 })
+
+app.get("/ads/:id/discord", (request:any, response:any) => {
+    return response.json([
+        {id:1, name:"Anúncio 1"},
+        {id:2, name:"Anúncio 2"},
+        {id:3, name:"Anúncio 3"},
+        {id:4, name:"Anúncio 4"}
+    ])
+})
+
 app.listen(3333)
