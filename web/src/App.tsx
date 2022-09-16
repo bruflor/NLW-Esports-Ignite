aaecba6ab19eac129bdba0ad3389f39d352cfe2b
Bruna Flor <brn.flor@gmail.com>
2022-09-16 20:50:44 +0100

    new components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,24 @@
 import "./styles/main.css";
 import logoImg from "./assets/logo-nlw-esports.png";
-import { MagnifyingGlassPlus } from "phosphor-react";
+import { GameBanner } from "./components/GameBanner";
+import { CreateAdBanner } from "./components/CreateAdBanner";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} />
+
       <h1 className="text-6xl text-white font-black mt-20">
         Seu&nbsp;
         <span className="text-transparent bg-nlw-gradient bg-clip-text">
@@ -12,34 +26,16 @@ function App() {
         </span>
         está aqui
       </h1>
-      <div className="grid grid-cols-6 gap-6 mt-16">
-        <a className="relative rounded-lg overflow-hidden" href="">
-          <img src="/game-1.png" alt="" />
-          <div className="bg-dark w-full pt-16 pb-4 px-4 bg-game-gradient  absolute bottom-0 right-0 left-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-      </div>
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-          <div>
-            <strong className="text-2xl text-white font-black block">
-              Não encontrou seu duo?
-            </strong>
-            <span className="text-zinc-400 block">
-              Publique um anúncio para encontrar novos players!
-            </span>
-          </div>
 
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <GameBanner
+          bannerUrl="/game-1.png"
+          title="League of Legends"
+          adsCount={0}
+        />
       </div>
+
+      <CreateAdBanner />
     </div>
   );
 }
