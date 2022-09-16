1639c0521c86368cf142b8039510802c9a823bba
Bruna Flor <brn.flor@gmail.com>
2022-09-16 21:37:47 +0100

    map for games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,14 +4,23 @@ import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
 import { useEffect, useState } from "react";
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
 function App() {
-  const [games, setGames] = useState([]);
+  const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:3333/games")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        setGames(data);
       });
   }, []);
 
@@ -28,11 +37,15 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner
-          bannerUrl="/game-1.png"
-          title="League of Legends"
-          adsCount={0}
-        />
+        {games.map((game) => {
+          return (
+            <GameBanner
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
       </div>
 
       <CreateAdBanner />
