cf31e0cf087dde01929400338e5ee73496a83c93
Bruna Flor <brn.flor@gmail.com>
2022-09-21 21:28:33 +0100

    games on mobile from backend

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -12,9 +12,9 @@ import { THEME } from "../../theme";
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: any;
+  title: string;
+  _count: { ads: number };
+  bannerUrl: string;
 }
 
 interface Props {
@@ -24,10 +24,10 @@ interface Props {
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground source={data.cover} style={styles.cover}>
+      <ImageBackground source={{ uri: data.bannerUrl }} style={styles.cover}>
         <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
-          <Text style={styles.name}>{data.name}</Text>
-          <Text style={styles.ads}>{data.ads} anúncios</Text>
+          <Text style={styles.name}>{data.title}</Text>
+          <Text style={styles.ads}>{data._count.ads} anúncios</Text>
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { View, Image, FlatList, Text } from "react-native";
-
 import { styles } from "./styles";
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { Heading } from "../../components/Heading";
-import { GameCard } from "../../components/GameCard";
-import { GAMES } from "../../utils/games";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  useEffect(() => {
+    fetch("http://192.168.1.68:3333/games").then((response) =>
+      response.json().then((data) => setGames(data))
+    );
+  }, []);
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -17,7 +22,7 @@ export function Home() {
         subtitle={"selecione o game que deseja jogar..."}
       />
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         horizontal
